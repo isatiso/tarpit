b8eac7a6da5d55e193e3c7dba880414c7e856de7
plank <sieglive@gmail.com>
2019-03-21 01:17:50 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tarpit',
-    version='0.0.3',
+    version='0.0.4',
     author='plank',
     author_email='sieglive@gmail.com',
     url='https://lazor.cn',
